cc0a4ea78ff151062a2b660499f0c1fab7d7e3fe
Yihua Hao <30879453+yihuaH@users.noreply.github.com>
2022-03-25 14:57:53 -0700

    013

---
+++ 0013. Roman to Integer.cpp
@@ -0,0 +1,63 @@
+#include "leetcode.h"
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        int value = 0;
+        for (size_t i = 0; i < s.size(); i++)
+        {
+            switch (s[i])
+            {
+            case 'M':
+                value += 1000;
+                break;
+            case 'D':
+                value += 500;
+                break;
+            case 'C':
+                value += 100;
+                if(s[i+1]=='D'||s[i+1]=='M'){
+                    i++;
+                    if(s[i]=='D'){
+                        value += 300;
+                    }else{
+                        value += 800;
+                    }
+                }
+                break;
+            case 'L':
+                value += 50;
+                break;
+            case 'X':
+                value += 10;
+                if(s[i+1]=='L'||s[i+1]=='C'){
+                    i++;
+                    if(s[i]=='L'){
+                        value += 30;
+                    }else{
+                        value += 80;
+                    }
+                }
+                break;
+            case 'V':
+                value += 5;
+                break;
+            case 'I':
+                value += 1;
+                if(s[i+1]=='V'||s[i+1]=='X'){
+                    i++;
+                    if(s[i]=='V'){
+                        value += 3;
+                    }else{
+                        value += 8;
+                    }
+                }
+                break;
+            default:
+                break;
+            }
+        }
+        return value;
+    }
+};
