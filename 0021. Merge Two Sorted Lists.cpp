1533b84b90d6674824fe4caa7d4145c64d0101a7
Yihua Hao <30879453+yihuaH@users.noreply.github.com>
2022-04-04 00:01:59 -0700

    clion commit

--- 0021. Merge Two Sorted Lists.cpp
+++ 0021. Merge Two Sorted Lists.cpp
@@ -22,15 +22,12 @@ public:
             return list1;
         }
 
-        while (list1->next!=nullptr && list2->next!=nullptr)
-        {
-            if(list1->val <= list2->val){
-                list1->next = mergeTwoLists(list1->next,list2);
-                return list1;
-            }else{
-                list2->next = mergeTwoLists(list2->next,list1);
-                return list2;
-            }
+        if(list1->val <= list2->val){
+            list1->next = mergeTwoLists(list1->next,list2);
+            return list1;
+        }else{
+            list2->next = mergeTwoLists(list2->next,list1);
+            return list2;
         }
     }
 };
