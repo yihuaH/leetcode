9d420c9345983d92b0032dd3fc018e38b9cc5d7f
Yihua Hao <30879453+yihuaH@users.noreply.github.com>
2022-06-05 21:49:14 -0700

    1752

--- 1752.check-if-array-is-sorted-and-rotated.cpp
+++ 1752.check-if-array-is-sorted-and-rotated.cpp
@@ -5,9 +5,41 @@
  */
 
 // @lc code=start
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     bool check(vector<int>& nums) {
+        bool ans = true;
+        int flag;
+        vector<int> new_nums;
+        for (size_t i = 0; i < nums.size()-1; i++)
+        {
+            if (nums[i+1]<nums[i])
+            {
+                flag = i+1;
+                break;
+            }
+        }
+        for (size_t i = flag; i < nums.size(); i++)
+        {
+            new_nums.push_back(nums[i]);
+        }
+        for (size_t i = 0; i < flag; i++)
+        {
+            new_nums.push_back(nums[i]);
+        }
+        for (size_t i = 0; i < new_nums.size()-1; i++)
+        {
+            if (new_nums[i+1]<new_nums[i])
+            {
+                ans = false;
+                break;
+            }
+        }
+        return ans;
+        
         
     }
 };
