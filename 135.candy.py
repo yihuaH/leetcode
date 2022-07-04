cacd6db7273ff41828e3835cdb79e0faef7f2560
Yihua Hao <30879453+yihuaH@users.noreply.github.com>
2022-07-03 19:03:17 -0700

    135

---
+++ 135.candy.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=135 lang=python3
+#
+# [135] Candy
+#
+from typing import *
+# @lc code=start
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n, ans = len(ratings), [1]*len(ratings)
+        
+        for i in range(n-1):
+            if ratings[i] < ratings[i+1]:
+                ans[i+1] = max(1 + ans[i], ans[i+1])
+                
+        for i in range(n-2, -1, -1):
+            if ratings[i+1] < ratings[i]:
+                ans[i] = max(1 + ans[i+1], ans[i])
+        
+        return sum(ans)
+            
+# @lc code=end
+
