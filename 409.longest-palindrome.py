7b2c14a92752749f936f4adc2e14394945817a2b
Yihua Hao <30879453+yihuaH@users.noreply.github.com>
2022-07-03 20:27:35 -0700

    409

--- 409.longest-palindrome.py
+++ 409.longest-palindrome.py
@@ -9,19 +9,22 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         if len(s) == 1:
             return 1
-        a = list(set(s))
+        a = list(set(s))#去重
         ans = 0
-        flag = 0
+        flag = 0#可以加一次奇数
         print(a)
         for i in a:
             temp = s.count(i)
+            #如果是偶数可以直接加入到答案里面
             if temp % 2 == 0:
                 ans += temp
             else: 
+                #如果没有加过奇数，可以加一次奇数
                 if flag == 0:
                     flag = 1
                     ans += temp
                 else:
+                    #如果加过奇数后，并且是大于二的奇数，可以加这个数减一
                     if temp > 2:
                         ans += temp - 1
         return ans
