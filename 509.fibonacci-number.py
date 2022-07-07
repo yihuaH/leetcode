a69496390057ee3dc1d4190832749d9aa55302be
Yihua Hao <30879453+yihuaH@users.noreply.github.com>
2022-07-06 18:37:14 -0700

    509

---
+++ 509.fibonacci-number.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=509 lang=python3
+#
+# [509] Fibonacci Number
+#
+
+# @lc code=start
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        dp = [None] * (n+1)
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(2,n+1):
+            dp[i] = dp[i-1]+dp[i-2]
+        return dp[n]
+# @lc code=end
+
