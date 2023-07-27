62cf38ea0f26f9bff8b167b3b1a84a7c22cf1019
yihuaH <haoyihua88@gmail.com>
2023-07-27 12:21:43 -0700

    ini

---
+++ 441.arranging-coins.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=441 lang=cpp
+ *
+ * [441] Arranging Coins
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        
+    }
+};
+// @lc code=end
+
