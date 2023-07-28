a33f22184b2fcc11d8dd31f11356df4adb5d3a04
Yihua Hao <30879453+yihuaH@users.noreply.github.com>
2023-07-27 20:29:15 -0700

    finish

---
+++ 292.nim-game.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=292 lang=python3
+#
+# [292] Nim Game
+#
+
+# @lc code=start
+class Solution:
+    def canWinNim(self, n: int) -> bool:
+        if n % 4 == 0:
+            return False
+        else:
+            return True
+# @lc code=end
+
