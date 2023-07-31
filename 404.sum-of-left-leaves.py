8b321265508f9a38f9c799b3a0a22363647ec91c
yihuaH <haoyihua88@gmail.com>
2023-07-31 09:40:44 -0700

    ss

---
+++ 404.sum-of-left-leaves.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=404 lang=python3
+#
+# [404] Sum of Left Leaves
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        
+# @lc code=end
+
